994f1a4bfa16a19283e0af846cfed3e777e01383
marcos-renato <marcos.vicente@mrpv.org>
2023-10-16 13:28:36 -0300

    atualizando classe paiscontroler busncando por id

--- transiteasyServRest/src/controllers/PaisControler.js
+++ transiteasyServRest/src/controllers/PaisControler.js
@@ -20,6 +20,15 @@ const criarPais = async (req, res, next) => {
   try {
     // Use o modelo para criar um novo registro
     const newPais = await Pais.create(req.body);
+    const criarPais = async (req, res, next) => {
+      try {
+        const novoPais = await Pais.create(req.body);
+        res.status(201).json(novoPais);
+      } catch (error) {
+        next(error);
+      }
+    };
+    
     res.status(201).json(newPais);
   } catch (error) {
     next(error);
@@ -28,10 +37,33 @@ const criarPais = async (req, res, next) => {
 
 const atualizarPais = (req, res, next) => {
   // Sua lógica de atualização aqui
+  const atualizarPais = async (req, res, next) => {
+    const idPais = req.params.id; // Supondo que você passa o ID do país como parte da URL.
+    try {
+      await Pais.update(req.body, {
+        where: { idPais },
+      });
+      res.status(200).json({ message: 'País atualizado com sucesso.' });
+    } catch (error) {
+      next(error);
+    }
+  };
+  
 };
 
 const excluirPais = (req, res, next) => {
   // Sua lógica de exclusão aqui
+  const excluirPais = async (req, res, next) => {
+    const idPais = req.params.id; // Supondo que você passa o ID do país como parte da URL.
+    try {
+      await Pais.destroy({
+        where: { idPais },
+      });
+      res.status(200).json({ message: 'País excluído com sucesso.' });
+    } catch (error) {
+      next(error);
+    }
+  }; 
 };
 
 const listarPaises = async (req, res, next) => {
@@ -48,8 +80,19 @@ const listarPaises = async (req, res, next) => {
   }
 };
 
-const obterPaisPorId = (req, res, next) => {
+const obterPaisPorId = async (req, res, next) => {
   // Sua lógica para obter um país por ID aqui
+  let id = req.params.id;
+  try {
+    const query = `select p.idPais ,p.nmPais, p.snAtivo from transiteasydb.pais p where p.idPais=${id}`;
+    const results = await sequelize.query(query, { type: Sequelize.QueryTypes.SELECT });
+
+    // Enviar os resultados como resposta
+    res.json(results);
+  } catch (error) {
+    console.error('Erro ao executar a consulta:', error);
+    res.status(500).json({ error: 'Erro ao executar ao listar os paises.' });
+  }
 };
 
 module.exports = {
